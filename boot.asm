a34830c5ded87df591f25066806439bb5cf81f93
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2025-01-05 17:40:24 -0300

    alterado rotina para que seja preenchido cores

--- boot.asm
+++ boot.asm
@@ -1,18 +1,43 @@
-ORG 0X7C00
-BITS 16
+[ORG 0X7C00]
+[BITS 16]
+
+MOV AH,0x00     ; Define mode video
+MOV AL,0x13      
+INT 0x10        
 
 
-;define mode video
-mov ah,0x00
-mov al,0x13
-int 0x10
-
+MOV AL, 0x05
+MOV BX, 0x00       ; Page 0
+MOV CX, 0x00       ; Column 
+MOV DX, 0x00       ; Row
+ 
 _LOOP:
-    jmp _LOOP
+    MOV AH,0x0C 
+    INT 0x10
 
-mov ah,0x0C
-int 0x10
+    INC CX
+    CMP CX,0x0140
+    JNE _LOOP
 
+    MOV CX,0x00
+    INC DX
+    CMP DX,0xC8
+    JNE _LOOP
+    MOV DX,0x00
+    INC AL
+    
+    CMP AL,0xFF
+    JE RESET_COLOR
+    JMP _LOOP
+
+RESET_COLOR:
+    MOV AL, 0x00
+    JMP _LOOP
+
+PRINT_COLOR:
+    MOV AH,0x0C
+    INT 0x10
+    RET
 
 times 510-($-$$) db 0
 
